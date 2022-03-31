d513beb7b26f1077a5f6739a1a9213943ff30319
PHH <phouhua@gmail.com>
2022-03-31 16:39:09 +0800

    commit four

--- student.java
+++ student.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class student{
-	   string name;
-	   string address;
-	   string email;   //修改的内容
-
+	private String name;
+	private String address;
+	private String email;   //修改的内容
+	private String telephone;   //新增电话属性
 }
